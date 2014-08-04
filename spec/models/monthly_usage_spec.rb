c460884d164cf371fd576592e0a06cd9c72a8901
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-08-04 16:39:35 -0400

    basic tests for these minimal models

--- spec/models/monthly_usage_spec.rb
+++ spec/models/monthly_usage_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe MonthlyUsage do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before { StripeMock.start }
+  after { StripeMock.stop }
+
+  let(:user) { FactoryGirl.create(:user) }
+
+  it "should create a valid entry" do
+    usage = MonthlyUsage.create!(use: 'test', entity: user, month: 1, year: 2014, value: 100)
+    usage.entity_id.should == user.id
+  end
+
 end
--- spec/models/speaker_spec.rb
+++ spec/models/speaker_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Speaker do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+
+  let(:user) { FactoryGirl.create :user }
+  let(:transcript) { FactoryGirl.create :transcript }
+  before { StripeMock.start }
+  after { StripeMock.stop }
+
+  it "creates a valid speaker" do
+    speaker = Speaker.create!(name: 'M1', times: [[0, 120]], transcript: transcript)
+  end
+
 end
