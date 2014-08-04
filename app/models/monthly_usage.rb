5c9a18356349c7e00b429232afb2fd454426906c
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-08-04 16:38:04 -0400

    basic tests for these minimal models

--- app/models/monthly_usage.rb
+++ app/models/monthly_usage.rb
@@ -1,5 +1,8 @@
 class MonthlyUsage < ActiveRecord::Base
 
   belongs_to :entity, :polymorphic => true
+  attr_accessible :entity, :entity_id, :entity_type, :month, :year, :use, :value
+
+  PREMIUM_TRANSCRIPTS = 'premium transcripts'
 
 end
--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -2,7 +2,7 @@ class Speaker < ActiveRecord::Base
 
   serialize :times
 
-  attr_accessible :name, :times
+  attr_accessible :name, :times, :transcript
 
   belongs_to :transcript
   has_many :timed_texts
