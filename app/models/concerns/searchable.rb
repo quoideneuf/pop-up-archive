64b8a5181ac7967fa77bcfeff261ec6fbdcb1712
Yury Polyakov <x3qt.dev@gmail.com>
2014-09-03 22:26:29 +0300

    Update searchable callbacks

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -6,12 +6,19 @@ module Searchable
 
     # Set up callbacks for updating the index on model changes
     #
-    after_commit lambda { Indexer.perform_async(:index,  self.class.to_s, self.id) }, on: :create, if: Proc.new { |item| item.responds_to?(:deleted_at) && item.deleted_at.blank? }
+    after_commit lambda { IndexerWorker.perform_async(:index,  self.class.to_s, self.id) },
+      on: :create,
+      if: Proc.new { |item| item.respond_to?(:deleted_at) && item.deleted_at.blank? }
 
-    after_commit lambda { Indexer.perform_async(:update, self.class.to_s, self.id) }, on: :update, if: Proc.new { |item| item.responds_to?(:deleted_at) && item.deleted_at.blank? }
+    after_commit lambda { IndexerWorker.perform_async(:update, self.class.to_s, self.id) },
+      on: :update,
+      if: Proc.new { |item| item.respond_to?(:deleted_at) && item.deleted_at.blank? }
 
-    after_commit lambda { Indexer.perform_async(:delete, self.class.to_s, self.id) }, on: :destroy, if: Proc.new { |item| item.responds_to?(:deleted_at) && item.deleted_at.blank? }
+    after_commit lambda { IndexerWorker.perform_async(:delete, self.class.to_s, self.id) },
+      on: :destroy,
+      if: Proc.new { |item| item.respond_to?(:deleted_at) && item.deleted_at.blank? }
     
-    after_touch  lambda { Indexer.perform_async(:update, self.class.to_s, self.id) }, if: Proc.new { |item| item.responds_to?(:deleted_at) && item.deleted_at.blank? }
+    after_touch  lambda { IndexerWorker.perform_async(:update, self.class.to_s, self.id) },
+      if: Proc.new { |item| item.respond_to?(:deleted_at) && item.deleted_at.blank? }
   end
 end
