40509f033fca074c231f19e3ed2377fd282ecc25
Yury Polyakov <x3qt.dev@gmail.com>
2014-09-03 20:44:19 +0300

    Update concern searchable to check if there are deleted_at field

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -6,9 +6,12 @@ module Searchable
 
     # Set up callbacks for updating the index on model changes
     #
-    after_commit lambda { Indexer.perform_async(:index,  self.class.to_s, self.id) }, on: :create
-    after_commit lambda { Indexer.perform_async(:update, self.class.to_s, self.id) }, on: :update
-    after_commit lambda { Indexer.perform_async(:delete, self.class.to_s, self.id) }, on: :destroy
-    after_touch  lambda { Indexer.perform_async(:update, self.class.to_s, self.id) }
+    after_commit lambda { Indexer.perform_async(:index,  self.class.to_s, self.id) }, on: :create, if: Proc.new { |item| item.responds_to?(:deleted_at) && item.deleted_at.blank? }
+
+    after_commit lambda { Indexer.perform_async(:update, self.class.to_s, self.id) }, on: :update, if: Proc.new { |item| item.responds_to?(:deleted_at) && item.deleted_at.blank? }
+
+    after_commit lambda { Indexer.perform_async(:delete, self.class.to_s, self.id) }, on: :destroy, if: Proc.new { |item| item.responds_to?(:deleted_at) && item.deleted_at.blank? }
+    
+    after_touch  lambda { Indexer.perform_async(:update, self.class.to_s, self.id) }, if: Proc.new { |item| item.responds_to?(:deleted_at) && item.deleted_at.blank? }
   end
 end
