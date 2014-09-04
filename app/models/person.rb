0f8a42c023d6105ff683b61467dc5b14ed1e8979
Yury Polyakov <x3qt.dev@gmail.com>
2014-09-04 23:30:15 +0300

    Comment as_json person method

--- app/models/person.rb
+++ app/models/person.rb
@@ -55,9 +55,9 @@ class Person < ActiveRecord::Base
     find_by_slug slugify string or create name: string
   end
 
-  def as_json(params={})
-    name.as_json
-  end
+  # def as_json(params={})
+  #   name.as_json
+  # end
 
   private
 
