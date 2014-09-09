7ea62e3f9b9f606cb35c40a79b04edeae657001b
Yury Polyakov <x3qt.dev@gmail.com>
2014-09-10 01:20:23 +0300

    Add search dsl

---
+++ app/models/search/filter.rb
@@ -0,0 +1,22 @@
+# http://www.elasticsearch.org/guide/reference/api/search/filter.html
+# http://www.elasticsearch.org/guide/reference/query-dsl/
+#
+class Search::Filter
+
+  def initialize(type, *options)
+    value = if options.size < 2
+      options.first || {}
+    else
+      options # An +or+ filter encodes multiple filters as an array
+    end
+    @hash = { type => value }
+  end
+
+  def to_json(options={})
+    to_hash.to_json
+  end
+
+  def to_hash
+    @hash
+  end
+end
