5fe696066b1ce95126eaa99b6b9d79db5e92b7dc
Peter Karman <peter@popuparchive.com>
2014-09-30 10:53:45 -0500

    more debugging

--- app/models/item_results_presenter.rb
+++ app/models/item_results_presenter.rb
@@ -38,6 +38,7 @@ class ItemResultsPresenter < BasicObject
 
         # child objects
         fres[:audio_files] = result.audio_files.map do |af|
+          $stderr.puts "audio_file:"
           $stderr.puts ::PP::pp(af)
           { :url => af.url, :id => af.id, :filename => af.filename } 
         end
