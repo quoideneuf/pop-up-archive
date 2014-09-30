8a2c0d43dbe62bce822134f6685fcf35d1be3dbf
Peter Karman <peter@popuparchive.com>
2014-09-29 21:52:54 -0500

    refactor ES logging (again) to respect ES_DEBUG env var

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -4,16 +4,20 @@ es_url = 'http://localhost:9200'
 if ENV['BONSAI_URL'] || ENV['ELASTICSEARCH_URL']
   es_url = ENV['BONSAI_URL'] || ENV['ELASTICSEARCH_URL']
 end
-Elasticsearch::Model.client = Elasticsearch::Client.new({
-  url: es_url,
-  log: true,
-  trace: true
-})
 
-if Rails.application.config.elasticsearch_logging || ENV['DEBUG']
+if Rails.application.config.elasticsearch_logging || ENV['ES_DEBUG'].to_i > 0
   logger = Logger.new(STDOUT)
   logger.level =  Logger::DEBUG
-  Elasticsearch::Model.client.transport.logger = logger
+  Elasticsearch::Model.client = Elasticsearch::Client.new({
+    url: es_url,
+    log: true,
+    logger: logger,
+  })
+  puts "Elasticsearch logging set to DEBUG mode"
+else 
+  Elasticsearch::Model.client = Elasticsearch::Client.new({
+    url: es_url,
+  })
 end
 
 puts "Starting Elasticsearch model with server #{es_url}"
