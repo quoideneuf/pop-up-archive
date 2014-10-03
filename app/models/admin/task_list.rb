74d9fd3dbbdf482c834c91962926c9598a48e4c5
Peter Karman <peter@popuparchive.com>
2014-10-03 13:25:06 -0400

    more re #593: workaround case where collection is AWOL

--- app/models/admin/task_list.rb
+++ app/models/admin/task_list.rb
@@ -15,14 +15,25 @@ class Admin::TaskList < Admin::Report
         line[:owner_id] = task.owner_id
 
         # this is going to fail when we have collections owned by users and orgs - AK
-        audio_file = task.owner.class.joins(item: :collection).find(task.owner_id)
-        line[:user_email] = audio_file.user.email
-        line[:user_id] = audio_file.user_id
-
-        line[:collection_title] = audio_file.item.collection.title
-        line[:collection_id] = audio_file.item.collection_id
-        line[:item_id] = audio_file.item_id
-        line[:item_title] = audio_file.item.title
+        if task.owner.class.name == 'AudioFile'
+          begin
+            audio_file = task.owner.class.joins(item: :collection).find(task.owner_id)
+          rescue
+            Rails.logger.warn("Cannot find audio file with item+collection for #{task.owner_id}")
+          end
+          if audio_file and !audio_file.user_id
+            Rails.logger.warn("No user_id on audio_file #{audio_file.id}")
+          elsif !audio_file
+            Rails.logger.warn("No audio_file #{task.owner_id}")
+          else
+            line[:user_email] = audio_file.user.email
+            line[:user_id] = audio_file.user_id
+            line[:collection_title] = audio_file.item.collection.title
+            line[:collection_id] = audio_file.item.collection_id
+            line[:item_id] = audio_file.item_id
+            line[:item_title] = audio_file.item.title
+          end
+        end
       end
 
       line[:created_at] = task.created_at
