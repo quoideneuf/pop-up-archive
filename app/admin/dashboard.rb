e1205c439b1f6aee7bce07586530471f8a3e6436
Peter Karman <peter@popuparchive.com>
2014-10-06 10:42:37 -0400

    add real dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,25 @@ ActiveAdmin.register_page "Dashboard" do
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
-    div :class => "blank_slate_container", :id => "dashboard_default_message" do
-      span :class => "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
+    columns do
+      column do
+        panel "Recent Collections" do
+          table do
+            Collection.order('created_at DESC').limit(20).map do |coll|
+              tr do
+                td link_to(coll.title, superadmin_collection_path(coll))
+                td link_to(coll.creator.name, superadmin_user_path(coll.creator))
+              end 
+            end
+          end
+        end
+      end # column one
+      column do
+        panel "Info" do
+          para "Welcome to Pop Up Archive SuperAdmin section."
+        end
+      end # column two
     end
+  end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
 end
