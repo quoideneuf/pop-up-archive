b33113ed45b8b206c49aad33cec17507c4e4cac0
Peter Karman <peter@popuparchive.com>
2014-10-07 21:46:25 -0500

    when using Stripe in test env/mode, return a generic community Customer object so that production snapshot users work in non-production envs

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,17 +1,26 @@
 class Customer
   attr_reader :id, :plan_id, :card, :trial, :interval
 
-  def initialize(stripe_customer)
-    @id = stripe_customer.id
-    if stripe_customer.respond_to?(:subscription) && stripe_customer.subscription.present?
-      @plan_id = stripe_customer.subscription.plan.id
-      if stripe_customer.subscription.trial_end.present?
-        @trial = (Time.at(stripe_customer.subscription.trial_end).to_date - Date.today).to_i
-      else
-        @trial = 0
+  def initialize(stripe_customer=nil, *attrs)
+    if !stripe_customer.nil?
+      @id = stripe_customer.id
+      if stripe_customer.respond_to?(:subscription) && stripe_customer.subscription.present?
+        @plan_id = stripe_customer.subscription.plan.id
+        if stripe_customer.subscription.trial_end.present?
+          @trial = (Time.at(stripe_customer.subscription.trial_end).to_date - Date.today).to_i
+        else
+          @trial = 0
+        end
       end
+      @card = stripe_customer.respond_to?(:cards) ? stripe_customer.cards.data[0].as_json.try(:slice, *%w(last4 type exp_month exp_year)) : {}
+    else
+      hashref = attrs.is_a?(Array) ? attrs.first : attrs
+      @id = hashref[:id]
+      @card = hashref[:card] || {}
+      @plan_id = hashref[:plan_id]
+      @trial = hashref[:trial] || 0
+      @interval = hashref[:interval]
     end
-    @card = stripe_customer.respond_to?(:cards) ? stripe_customer.cards.data[0].as_json.try(:slice, *%w(last4 type exp_month exp_year)) : {}
   end
 
   def plan
@@ -38,5 +47,9 @@ class Customer
     cust.update_subscription(plan: SubscriptionPlanCached.community.id)
     Rails.cache.delete([:customer, :individual, cust.id])
     return SubscriptionPlanCached.community
+  end
+
+  def self.generic_community
+    return new(nil, :id => 'generic-community-customer', :plan_id => SubscriptionPlanCached.community.id)
   end
 end
