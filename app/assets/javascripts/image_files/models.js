e2e13f9f807d9ca425c612c82666f26d9c1d919f
Shindo Strzelczyk <shindostrz@gmail.com>
2014-10-15 14:30:00 -0700

    Changed the items path to 'container' so that the model can be used for both images and collections

--- app/assets/javascripts/image_files/models.js
+++ app/assets/javascripts/image_files/models.js
@@ -1,7 +1,8 @@
 angular.module('Directory.imageFiles.models', ['RailsModel', 'S3Upload'])
 .factory('ImageFile', ['$window', 'Model', 'S3Upload', '$http', function ($window, Model, S3Upload, $http) {
-  var ImageFile = Model({url:'/api/items/{{itemId}}/image_files/{{id}}', name: 'image_file', only: ['url', 'filename', 'remoteFileUrl']});
-
+  var ImageFile = Model({url:'/api/{{container}}/{{containerId}}/image_files/{{id}}', name: 'image_file', only: ['url', 'filename', 'remoteFileUrl']});
+                                                    // Change the 'items' part of the path to a 'container' so it can be
+                                                    // used by collections, and create the path in routes
   ImageFile.prototype.cleanFileName = function (fileName) {
     return fileName.replace(/[^a-z0-9\.]+/gi,'_');
   }
