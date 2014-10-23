3d4298d06854d510c80d35d292b04e63ebf0b0dd
Peter Karman <peter@popuparchive.com>
2014-10-22 23:48:13 -0500

    add yearmonth column to ease sorting #641

--- app/models/monthly_usage.rb
+++ app/models/monthly_usage.rb
@@ -1,9 +1,15 @@
 class MonthlyUsage < ActiveRecord::Base
 
   belongs_to :entity, :polymorphic => true
-  attr_accessible :entity, :entity_id, :entity_type, :month, :year, :use, :value
+  attr_accessible :entity, :entity_id, :entity_type, :month, :year, :use, :value, :yearmonth
 
   PREMIUM_TRANSCRIPTS = 'premium transcripts'
   BASIC_TRANSCRIPTS   = 'basic transcripts'
+
+  before_save :set_yearmonth
+
+  def set_yearmonth
+    self.yearmonth = sprintf("%d-%02d", year, month)
+  end
 
 end
