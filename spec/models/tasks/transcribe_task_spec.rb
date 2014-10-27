d52e60ec1b7cd66dbd8edf8e39d2fc5d0737050d
Peter Karman <peter@popuparchive.com>
2014-10-27 10:34:13 -0500

    refactor to test email delivery, avoiding action-at-a-distance with global var

--- spec/models/tasks/transcribe_task_spec.rb
+++ spec/models/tasks/transcribe_task_spec.rb
@@ -11,6 +11,8 @@ describe Tasks::TranscribeTask do
     @task = FactoryGirl.create :transcribe_task
     @task.owner = @audio_file
     @task.extras['user_id'] = @user.id
+    # reset global var
+    ActionMailer::Base.deliveries = []
   end
 
   context "transcripts" do
@@ -36,18 +38,12 @@ describe Tasks::TranscribeTask do
       transcript.confidence.should eq 0.5
     end
 
-    it "analyzes transcript on task finish" do
+    it "analyzes transcript and notifies user on task finish" do
       json = '[{"start_time":0,"end_time":9,"text":"from Wednesday January 30th 2013 the following is a replay of the radio doctor daily session in North Carolina House of Representatives","confidence":0.90355223},{"start_time":8,"end_time":17,"text":"tractor seat visitors","confidence":0.8770266}]'
 
       @task.should_receive(:get_file).and_return(json)
       @task.audio_file.should_receive(:analyze_transcript)
       @task.finish_task
-    end
-
-    # should come last to get total emails, since uses a global var.
-    # notify is triggered in @task.finish_task()
-    it "notifies user when transcript is finished" do
-      #STDERR.puts ActionMailer::Base.deliveries.inspect
       expect(ActionMailer::Base.deliveries.size).to eql(1)
     end
 
