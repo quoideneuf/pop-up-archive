ef218f935911b2f232a8138d9ecd31329470094a
Peter Karman <peter@popuparchive.com>
2014-10-27 10:05:54 -0500

    fix failing tests, ensuring task.extras.user_id is set (and adding email notification test for a bonus)

--- spec/factories/item_factory.rb
+++ spec/factories/item_factory.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :item do
     association :collection, factory: :collection_public
+    title "some test item title"
 
     factory :item_private do
       association :collection, factory: :collection_private
--- spec/models/tasks/transcribe_task_spec.rb
+++ spec/models/tasks/transcribe_task_spec.rb
@@ -7,7 +7,10 @@ describe Tasks::TranscribeTask do
 
   before(:each) do
     @audio_file = FactoryGirl.create :audio_file
-    @task = Tasks::TranscribeTask.new(owner: @audio_file, identifier: 'ts_all', extras: {'original' => "http://fakeurl.dev" } )
+    @user = FactoryGirl.create :user
+    @task = FactoryGirl.create :transcribe_task
+    @task.owner = @audio_file
+    @task.extras['user_id'] = @user.id
   end
 
   context "transcripts" do
@@ -36,10 +39,16 @@ describe Tasks::TranscribeTask do
     it "analyzes transcript on task finish" do
       json = '[{"start_time":0,"end_time":9,"text":"from Wednesday January 30th 2013 the following is a replay of the radio doctor daily session in North Carolina House of Representatives","confidence":0.90355223},{"start_time":8,"end_time":17,"text":"tractor seat visitors","confidence":0.8770266}]'
 
-      @task = FactoryGirl.create :transcribe_task
       @task.should_receive(:get_file).and_return(json)
       @task.audio_file.should_receive(:analyze_transcript)
       @task.finish_task
+    end
+
+    # should come last to get total emails, since uses a global var.
+    # notify is triggered in @task.finish_task()
+    it "notifies user when transcript is finished" do
+      #STDERR.puts ActionMailer::Base.deliveries.inspect
+      expect(ActionMailer::Base.deliveries.size).to eql(1)
     end
 
   end
