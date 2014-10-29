564c00baea557e3d1927a9a6017d9d55c9f46284
Peter Karman <peter@popuparchive.com>
2014-10-29 13:31:14 -0500

    use active_admin_sidebar gem instead of our custom lib

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -139,7 +139,9 @@ ActiveAdmin.setup do |config|
   # You can add before, after and around filters to all of your
   # Active Admin resources and pages from here.
   #
-  # config.before_filter :do_something_awesome
+  config.before_filter do
+    left_sidebar! if respond_to?(:left_sidebar!)
+  end
   
   
   # == Setting a Favicon
