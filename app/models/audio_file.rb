quoideneuf/pop-up-archive

commit 4af86866bc94b295acc6edba9a13aff2b1a07529

warn, not debug, on attempt to create dupe speechmatics task

app/models/audio_file.rb
@@ -214,8 +214,7 @@ class AudioFile < ActiveRecord::Base
     return if (duration.to_i <= 0)
 
     if task = tasks.any?{|task| task.type == 'Tasks::SpeechmaticsTranscribeTask' && task.status != "failed"}
-    # if task = tasks.speechmatics_transcribe.without_status(:failed).where(identifier: identifier).last
-      logger.debug "speechmatics transcribe task #{identifier} already exists for audio file #{self.id}"
+      logger.warn "speechmatics transcribe task #{task.id} #{identifier} already exists for audio file #{self.id}"
     else
       extras = { 'original' => process_file_url, 'user_id' => user.try(:id) }.merge(options)
       task = Tasks::SpeechmaticsTranscribeTask.new(identifier: identifier, extras: extras)

commit d7a695edfbe4603273079f22f983cb2913511c3a

fix regression introduced in 4af86866bc94b295acc6edba9a13aff2b1a07529

app/models/audio_file.rb
@@ -218,7 +218,7 @@ class AudioFile < ActiveRecord::Base
   def start_premium_transcribe_job(user, identifier, options={})
     return if (duration.to_i <= 0)
 
-    if task = tasks.any?{|task| task.type == 'Tasks::SpeechmaticsTranscribeTask' && task.status != "failed"}
+    if task = tasks.select{|task| task.type == 'Tasks::SpeechmaticsTranscribeTask' && task.status != "failed"}.first
       logger.warn "speechmatics transcribe task #{task.id} #{identifier} already exists for audio file #{self.id}"
     else
       extras = { 'original' => process_file_url, 'user_id' => user.try(:id) }.merge(options)