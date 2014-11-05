6291bbbd874a7674730d9cd27cb8c1309bc95893
Peter Karman <peter@popuparchive.com>
2014-11-05 15:26:47 -0600

    fix typo

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -79,7 +79,7 @@ PopUpArchive::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.action_mailer.default_url_options = { :host => 'pop-up-archive-staging.herokuapp.com', :protocol => 'https' }
-  Rails.application.routes.default_url_options = { host: 'pop-up-archive-staging.herokuapp.com', protocol: => 'https' }
+  Rails.application.routes.default_url_options = { host: 'pop-up-archive-staging.herokuapp.com', protocol: 'https' }
 
   require 'autoscaler/sidekiq'
   require 'autoscaler/heroku_scaler'
