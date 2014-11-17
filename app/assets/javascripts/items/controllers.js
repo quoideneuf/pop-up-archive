d79ab80b898250d0be8b207f5cf74628f1ab47ed
Shindo Strzelczyk <shindostrz@gmail.com>
2014-11-17 12:46:47 -0800

    Changed the statusNotification function to use the currentStatus property on the audio file

--- app/assets/javascripts/items/controllers.js
+++ app/assets/javascripts/items/controllers.js
@@ -61,57 +61,16 @@ angular.module('Directory.items.controllers', ['Directory.loader', 'Directory.us
     return (user && item && user.canEdit(item) && (file.transcript == null));
   };
 
-  $scope.status = "uploading";
-  $scope.upgradePlan = false;
-
   $scope.statusNotification = function(file) {
-    $scope.taskStatus(file);
-    var statusHTML = "<h4>Status: ";
-    if ($scope.status == "uploading") {
-      statusHTML += "uploading";
-    } else if ($scope.status == "upload_failed") {
-      statusHTML += "UPLOAD FAILED - please try uploading again or contact us if uploads keep failing.";
-    } else if ($scope.status == "started" || $scope.status == "full") {
-      statusHTML += "transcript processing.</h4>";
-      if ($scope.upgradePlan) {
-        statusHTML += "<p>The first two minutes of your transcription will be ready momentarily. <a href='/pricing'>Upgrade your plan for full transcripts.</a></p>";
-      } else if ($scope.transcriptType == "Basic" && !$scope.upgradePlan) {
-        statusHTML += "<p>The first two minutes of your transcription will be ready momentarily.";
-      }
+    var statusHTML = "<h4>Status: " + file.currentStatus + "</h4>";
+    if (file.currentStatus == "Transcript Preview processing") {
+      statusHTML += "<p>The first two minutes of your transcription will be ready momentarily. ";
+      } if ($scope.currentUser.hasCommunityPlan) {
+        statusHTML += "<a href='/pricing'>Upgrade your plan for full transcripts.</a></p>";
+      } else {
       statusHTML += "The full transcript will process in real time (a 30-minute file will take at least 30 minutes). We'll email you when it's ready.</p>";
-    } else if ($scope.status == "ts_failed") {
-      statusHTML += "TRANSCRIPTION FAILED - please <a href='mailto:edison@popuparchive.com?Subject=Transcription%20Failed%20-%20My%20User%20ID:%20"+ $scope.currentUser.id +"'>email us for support</a>";
     }
     return statusHTML;
-  };
-
-  $scope.taskStatus = function(file) {
-    if (!file) { return false; }
-    var upload, start, full;
-    for (var i=0; i<file.tasks.length; i++) {
-      var task = file.tasks[i];
-      if (task.type == "upload") {
-        upload = task.status;
-      } else if (task.identifier == "ts_start") {
-        start = task.status;
-      } else if (task.identifier == "ts_all" || task.identifier == "ts_paid") {
-        full = task.status;
-      }
-    }
-    if (upload == "failed") {
-      $scope.status = "upload_failed";
-    } else if (start == "failed" || full == "failed"){
-      $scope.status = "ts_failed";
-    } else if (start == "working" || start == "created") {
-      $scope.status = "started";
-    } else if (full != "complete") {
-      $scope.status = "full";
-    } else if (full == "complete") {
-      $scope.status = "finished";
-    }
-    if (!full) {
-      $scope.upgradePlan = true;
-    }
   };
 
   $scope.allowEditButton = function(file) {
@@ -121,7 +80,7 @@ angular.module('Directory.items.controllers', ['Directory.loader', 'Directory.us
       var found = false;
     }
     else {
-      if ($scope.status == "finished") {
+      if (file.currentStatus == "Basic Transcript complete" || file.currentStatus == "Premium Transcript complete") {
           var found = false;
       }
     }
