f5ff1763170975aebb1eae2d525ca2ca37a85b15
Peter Karman <peter@popuparchive.com>
2014-11-21 15:15:35 -0600

    failure to GET a collection returns 404 rather than 200 with empty json #713

--- app/controllers/api/v1/collections_controller.rb
+++ app/controllers/api/v1/collections_controller.rb
@@ -8,9 +8,16 @@ class Api::V1::CollectionsController < Api::V1::BaseController
   end
   expose :kollection do
     if params[:id]
+      koll = nil
       if user_signed_in?
-        current_user.collections.find_by_id(params[:id]) 
-      end || Collection.is_public.find_by_id(params[:id])
+        koll = current_user.collections.find_by_id(params[:id])
+      end
+      koll ||= Collection.is_public.find_by_id(params[:id])
+      # request for a specific collection failed. assume not found rather than unauthz.
+      if !koll
+        raise ActiveRecord::RecordNotFound.new "Cannot fetch collection with id #{params[:id]}"
+      end
+      koll
     else
       Collection.new(params[:collection].merge(creator: current_user))
     end
