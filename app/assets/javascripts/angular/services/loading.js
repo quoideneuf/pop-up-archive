quoideneuf/pop-up-archive

commit 43ed54ac8f5747089f01bc94bccc3751b15778fd

add error routes for internal API non-2xx response
This is less than ideal but the only way I could see to handle
error conditions, via http intercept. All error messages now
have their own template.

Addresses #595 and #713

app/assets/javascripts/angular/services/loading.js
@@ -37,7 +37,7 @@
     };
     $httpProvider.defaults.transformRequest.push(setIsLoading);
   }])
-  .factory('myHttpInterceptor', ['$q', '$rootScope', function ($q, $rootScope) {
+  .factory('myHttpInterceptor', ['$q', '$rootScope', '$location', function ($q, $rootScope, $location) {
     if (typeof $rootScope.setIsLoading === 'undefined') {
       $realRootScope = $rootScope;
       $rootScope.loading = loading;
@@ -45,11 +45,14 @@
     }
 
     return function (promise) {
-      return promise.then(function (response) {
+      return promise.then(function success(response) {
         actuallyIsLoading -= 1;
         if (actuallyIsLoading < 0) actuallyIsLoading = 0;
         return response;
-      }, function (response) {
+      }, function error(response) {
+        var errCode = response.status;
+        $rootScope.errorLocation = response.config.url;
+        $location.path('/error/' + errCode).search({was:response.config.url})
         actuallyIsLoading -= 1;
         if (actuallyIsLoading < 0) actuallyIsLoading = 0;
         return $q.reject(response);

app/assets/html/error/401.html.slim
@@ -1,0 +1,4 @@
+h1 This page requires you to log in.
+.error ng-show="errorLocation"
+  | The request was for 
+  span.error-location {{errorLocation}}

app/assets/html/error/403.html.slim
@@ -1,0 +1,6 @@
+h1 This page is private
+.error
+  | If you are the owner, make sure you are logged in and try again.
+.error ng-show="errorLocation"
+  | The request was for 
+  span.error-location {{errorLocation}}

app/assets/html/error/404.html.slim
@@ -1,0 +1,4 @@
+h1 Sorry, we couldn't find what you were looking for.
+.error ng-show="errorLocation"
+  | The request was for 
+  span.error-location {{errorLocation}}

app/assets/html/error/500.html.slim
@@ -1,0 +1,8 @@
+h1 Oops. The server had problems.
+.error
+  | Try reloading the page. If you continue to experience problems, please
+  a href="https://popuparchiveorg.zendesk.com/hc/en-us/requests/new"  let us know
+  | .
+.error ng-show="errorLocation"
+  | The request was for 
+  span.error-location {{errorLocation}}

app/assets/javascripts/directory/application.js.erb
@@ -211,6 +211,22 @@ Law, Silicon Valley, public media organizations, archives, standards-making bodi
       templateUrl: "<%= asset_path 'analytics/index.html' %>",
       title: "Analytics"
     })
+    .when('/error/404', {
+      templateUrl: "<%= asset_path 'error/404.html' %>",
+      title: "Not Found"
+    })
+    .when('/error/403', {
+      templateUrl: "<%= asset_path 'error/403.html' %>",
+      title: "Permission Denied"
+    })
+    .when('/error/401', {
+      templateUrl: "<%= asset_path 'error/401.html' %>",
+      title: "Login Required"
+    })
+    .when('/error/500', {
+      templateUrl: "<%= asset_path 'error/500.html' %>",
+      title: "Server Error"
+    })
     .otherwise({
       template: "<h1>404 - not found</h1>"
     });

commit 6395cc13a4da8b20521bc48ddc72a41b6056ff9c

only re-route on error if the window.location matches the xhr api call (fix regression introduced in 43ed54ac8f5747089f01bc94bccc3751b15778fd)

app/assets/javascripts/angular/services/loading.js
@@ -51,9 +51,15 @@
         return response;
       }, function error(response) {
         var errCode = response.status;
-        $rootScope.errorLocation = response.config.url;
-        $rootScope.prevLocation = $location.absUrl();
-        $location.path('/error/' + errCode).search({was:$rootScope.prevLocation})
+        // avoid recursion (error throwing error)
+        // and only re-route if this was the main request (matches window.location)
+        //console.log("error " + errCode + " for path " + $location.path() + " for response", response);
+        if (!$location.search().was && response.config.url == '/api' + $location.path()) {
+          console.log("redirect error " + errCode + " for response path " + response.config.url);
+          $rootScope.errorLocation = response.config.url;
+          $rootScope.prevLocation = $location.absUrl();
+          $location.path('/error/' + errCode).search({was:$rootScope.prevLocation})
+        }
         actuallyIsLoading -= 1;
         if (actuallyIsLoading < 0) actuallyIsLoading = 0;
         return $q.reject(response);