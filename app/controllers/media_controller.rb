b0cb9ac5b840fdb4ee785c9105ab28282a92caed
Peter Karman <peter@popuparchive.com>
2014-12-08 15:01:56 -0600

    fix image public url redirect, declaring version formats explicitly and testing for "thumb" in media controller

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -7,7 +7,11 @@ class MediaController < ApplicationController
     asset = params[:class].camelize.constantize.find(params[:id])
     if asset.public_url_token_valid?(params[:token], params)
       version = params[:extension] ? params[:extension].to_sym : nil
-      url = asset.url(version)
+      if params[:use] and params[:use] == "thumb"
+        url = asset.file.thumb.url
+      else 
+        url = asset.url(version)
+      end
       redirect_to url
     else
       head 401
