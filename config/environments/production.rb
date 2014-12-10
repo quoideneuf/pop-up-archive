a4c565f943c9f2decfb0d20d0c5da0bee1d22c76
Peter Karman <peter@popuparchive.com>
2014-12-10 16:24:13 -0600

    add tplayer assets to precompile list; tweek deploy message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ PopUpArchive::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += ['directory/base.css', 'directory/application.js', 'login/base.css', 'login.js', 'jplayer.popup.css', 'jquery.jplayer.js', 'pua_aa_stylesheet.css', 'jPlayer.css']
+  config.assets.precompile += ['directory/base.css', 'directory/application.js', 'login/base.css', 'login.js', 'jplayer.popup.css', 'jquery.jplayer.js', 'tplayer.js', 'pua_aa_stylesheet.css', 'jPlayer.css', 'tplayer.css']
 
   # Enable threaded mode
   # config.threadsafe!
