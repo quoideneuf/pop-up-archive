be0bfe94076fd453bd5f14c2eb6e40d8eab95154
Peter Karman <peter@popuparchive.com>
2014-12-17 09:56:16 -0600

    zap redundant recover! method

--- app/models/tasks/detect_derivatives_task.rb
+++ app/models/tasks/detect_derivatives_task.rb
@@ -15,17 +15,6 @@ class Tasks::DetectDerivativesTask < Task
     audio_file.update_attribute(:transcoded_at, DateTime.now)
   end
 
-  def recover!
-    if !audio_file
-      self.extras[:error] = 'No owner/audio_file assigned'
-      cancel!
-    else
-      # most often status is working but job has completed,
-      # and there's just a timing issue between the db commit and the worker running.
-      finish!
-    end 
-  end
-
   def urls
     deserialize_extra('urls', HashWithIndifferentAccess.new)
   end
