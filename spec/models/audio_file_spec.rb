ff2a885f244d4d72a1b870c0cc660413cee3babb
Peter Karman <peter@popuparchive.com>
2015-02-02 21:31:16 -0600

    now that audio_file.urls use permanent url reflect that in tests

--- spec/models/audio_file_spec.rb
+++ spec/models/audio_file_spec.rb
@@ -44,11 +44,11 @@ describe AudioFile do
 
     it "should provide a list of urls when transcoded" do
       @audio_file.transcoded_at = Time.now
-      @audio_file.urls.sort.should eq [@audio_file.public_url({:extension => :mp3}), @audio_file.public_url({:extension => :ogg})].sort
+      @audio_file.urls.sort.should eq [@audio_file.permanent_public_url({:extension => :mp3}), @audio_file.permanent_public_url({:extension => :ogg})].sort
     end
 
     it "should provide original url for urls when not transcoded" do
-      @audio_file.urls.should eq [@audio_file.public_url]
+      @audio_file.urls.should eq [@audio_file.permanent_public_url]
     end
 
     it "should provide url for private file" do
