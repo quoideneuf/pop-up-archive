149a2f8138eda524264bb5cb5ee3701bd9b2174e
Peter Karman <peter@popuparchive.com>
2015-02-06 13:13:54 -0600

    add collection.update_at to list view #982

--- app/admin/collection.rb
+++ app/admin/collection.rb
@@ -2,6 +2,7 @@ ActiveAdmin.register Collection do
   actions :index, :show
   index do
     column :title, sortable: :title do |coll| link_to coll.title, superadmin_collection_path(coll) end
+    column :updated_at
     column :default_storage, sortable: :default_storage do |coll| coll.storage end
     column :creator do |coll| coll.creator ? link_to(coll.creator) : '(none)' end
   end
@@ -13,7 +14,7 @@ ActiveAdmin.register Collection do
       attributes_table_for collection do
         row("ID") { collection.id }
         row("Title") { collection.title }
-        row("Creator") { collection.creator ? (link_to collection.creator.name, superadmin_user_path(collection.creator)) : '(none)' }
+        row("Creator") { collection.creator ? (link_to collection.creator.name, superadmin_user_path(collection.creator)) : nil }
         row("Billable To") { 
           if collection.billable_to.is_a?(User)
             link_to collection.billable_to.name, superadmin_user_path(collection.billable_to)
