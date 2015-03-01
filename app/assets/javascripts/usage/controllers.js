40ed59a2b209e3709548354fcb0418b6075fca11
Peter Karman <peter@popuparchive.com>
2015-02-28 21:36:35 -0600

    wip

--- app/assets/javascripts/usage/controllers.js
+++ app/assets/javascripts/usage/controllers.js
@@ -1,7 +1,5 @@
 angular.module('Directory.usage.controllers', ['Directory.usage.models'])
-.controller('UsageCtrl', ['$scope', 'Me', '$modal', function ($scope, Me, $modal) {
-  Me.authenticated(function (me) {
-    $scope.me = me;
-  });
+.controller('UsageCtrl', ['$scope', '$modal', function ($scope, $modal) {
 
+  
 }]);
--- app/assets/javascripts/usage/models.js
+++ app/assets/javascripts/usage/models.js
@@ -2,4 +2,7 @@ angular.module('Directory.usage.models', [])
 .factory('Usage', '$http', '$location', '$q', ['Model', function (Model, $http, $location, $q) {
   var Usage = new Model({url:'/api/usage/{{period}}', name: 'usage'});
 
+  console.log( Usage );
+
+  return Usage;
 }]);
