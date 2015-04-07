f921cea8d047653cb2ad42a6369f75b1fc45c698
Peter Karman <karpet@peknet.com>
2015-04-07 11:00:17 -0500

    decent_exposure choking a bit, so simplify code to help it along

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery
 
-  # decent_configuration do
-  #   strategy DecentExposure::StrongParametersStrategy
-  # end
+  decent_configuration do
+    strategy DecentExposure::StrongParametersStrategy
+  end
 
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = exception.message
