b7ccdbe0df5b70cd97c26fa863cd7d2531ed0539
Peter Karman <peter@popuparchive.com>
2015-04-11 14:12:15 -0500

    grasping at straws

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -22,6 +22,8 @@ PopUpArchive::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.assets.js_compressor = Uglifier.new(mangle: false)
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
