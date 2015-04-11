fead26d6fcede19014b37b47cbec4d62378e73b2
Peter Karman <peter@popuparchive.com>
2015-04-11 17:57:46 -0500

    debug nodejs err at heroku

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -16,13 +16,11 @@ PopUpArchive::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # IMPORTANT. Heroku needs this with Rails 4 so that /assets/html/* gets served correctly.
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
-
-  #config.assets.js_compressor = Uglifier.new(mangle: false)
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
