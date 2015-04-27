f2c6fe53e272d5cb089e0fcd63b3cbf195466607
Peter Karman <peter@popuparchive.com>
2015-04-27 11:16:34 -0500

    start on report for Stripe subscription events #1079

--- app/models/active_admin_comment.rb
+++ app/models/active_admin_comment.rb
@@ -3,4 +3,14 @@ class ActiveAdminComment < ActiveRecord::Base
   belongs_to :author, polymorphic: true
 
   attr_accessible :body, :namespace, :author_id, :author_type
+
+  def self.created_in_month(dtim=DateTime.now, namespace='superadmin')
+    month_start = dtim.utc.beginning_of_month
+    month_end = dtim.utc.end_of_month
+    start_dtim = month_start.strftime('%Y-%m-%d %H:%M:%S')
+    end_dtim   = month_end.strftime('%Y-%m-%d %H:%M:%S')
+    sql = "select * from active_admin_comments where namespace='#{namespace}' and created_at between '#{start_dtim}' and '#{end_dtim}' order by created_at desc"
+    ActiveAdminComment.find_by_sql(sql)
+  end
+
 end
