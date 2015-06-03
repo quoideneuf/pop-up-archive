8f32bbd9ee258fe642abde46b72d56b2ccb95664
Peter Karman <peter@popuparchive.com>
2015-06-03 11:41:57 -0500

    re-enable the dropdown template single-page-ness links with a special condition case for item.method #1126

--- app/assets/javascripts/angular/services/templates.js
+++ app/assets/javascripts/angular/services/templates.js
@@ -1,5 +1,5 @@
 // override some AngularStrap templates
 
 angular.module('mgcrea.ngStrap.dropdown').run([ '$templateCache', function($templateCache) {
-    $templateCache.put('dropdown/dropdown.tpl.html', '<ul tabindex="-1" class="dropdown-menu" role="menu"><li role="presentation" ng-class="{divider: item.divider}" ng-repeat="item in content"><a tabindex="-1" href="{{item.href}}" target="{{item.target || \'\'}}" data-method="{{item.method || \'get\'}}">{{item.text}}</a></li></ul>');
+    $templateCache.put('dropdown/dropdown.tpl.html', '<ul tabindex="-1" class="dropdown-menu" role="menu"><li role="presentation" ng-class="{divider: item.divider}" ng-repeat="item in content"><a role="menuitem" tabindex="-1" ng-href="{{item.href}}" ng-if="!item.divider && item.href && !item.method" target="{{item.target || \'\'}}" ng-bind="item.text"></a> <a tabindex="-1" ng-href="{{item.href}}" data-method="{{item.method || \'\'}}" ng-if="!item.divider && item.href && item.method" target="{{item.target || \'\'}}">{{item.text}}</a> <a role="menuitem" tabindex="-1" href="javascript:void(0)" ng-if="!item.divider && item.click" ng-click="$eval(item.click);$hide()" ng-bind="item.text"></a></li></ul>');
 } ]);
