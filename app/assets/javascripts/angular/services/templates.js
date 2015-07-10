quoideneuf/pop-up-archive

commit 5efb6a136bc5cfd67530a3c6f0b94c74dbba5ad2

refactor speaker selection to use its own template for bs-popover #1144

app/assets/javascripts/angular/services/templates.js
@@ -3,3 +3,7 @@
 angular.module('mgcrea.ngStrap.dropdown').run([ '$templateCache', function($templateCache) {
     $templateCache.put('dropdown/dropdown.tpl.html', '<ul tabindex="-1" class="dropdown-menu" role="menu"><li role="presentation" ng-class="{divider: item.divider}" ng-repeat="item in content"><a role="menuitem" tabindex="-1" ng-href="{{item.href}}" ng-if="!item.divider && item.href && !item.method" target="{{item.target || \'\'}}" ng-bind="item.text"></a> <a tabindex="-1" ng-href="{{item.href}}" data-method="{{item.method || \'\'}}" ng-if="!item.divider && item.href && item.method" target="{{item.target || \'\'}}">{{item.text}}</a> <a role="menuitem" tabindex="-1" href="javascript:void(0)" ng-if="!item.divider && item.click" ng-click="$eval(item.click);$hide()" ng-bind="item.text"></a></li></ul>');
 } ]);
+
+angular.module('mgcrea.ngStrap.popover').run([ '$templateCache', function($templateCache) {
+    $templateCache.put('popover/popover.tpl.html', '<div class="popover" style="min-width:150px"><div class="arrow"></div><button type="button" class="close" ng-click="$hide()" aria-hidden="true">&times;</button><h3 class="popover-title" ng-bind="title" ng-show="title"></h3><div class="popover-content" ng-bind-html="content"></div></div>');
+  } ]);

app/assets/html/items/speaker_picker.html.slim
@@ -1,0 +1,6 @@
+.popover style="min-width:150px"
+  .arrow
+  button type="button" class="close" ng-click="$hide()" aria-hidden="true"  &times;
+  h3 class="popover-title" Select Speaker
+  .popover-content
+    select ng-change='updateSpeaker(text, originalSpeaker, $index)' ng-model='text.speakerId' ng-options='speaker.id as speaker.name for speaker in speakers'

app/assets/javascripts/angular/services/player.js.erb
@@ -354,7 +354,7 @@
                       '<td ng-show="showStart">{{toTimestamp(text.startTime)}}</td>' +
                       '<td ng-hide="editRow" class="speaker" title="{{assignSpeaker(text.speakerId)}}" ng-bind="speakerChange(transcript[$index-1].speakerId, text.speakerId)"></td>' +
                       '<td ng-show="editRow && transcriptType != \'Basic\'">' +
-                          '<a bs-popover="speakerPopover"  data-placement="right" data-unique="1">' +
+                          '<a bs-popover data-template="<%= asset_path 'items/speaker_picker.html' %>" data-placement="right" data-unique="1">' +
                             '<span ng-bind="abbreviateSpeaker(text.speakerId)"></span>' +
                             '<i class="icon-chevron-down"></i>' +
                           ' </a>' +
@@ -565,11 +565,6 @@
             text.speakerId= speakerId;
             scope.updateSpeaker(text, originalSpeaker, index);
           }
-        };
-
-        scope.speakerPopover = { 
-          "title": "Select Speaker" + "<button onclick=\"$(this).closest('div.popover').popover('hide');\" type=\"button\" class=\"close\" aria-hidden=\"true\">&times;</button>",
-          "content": "<select ng-change='updateSpeaker(text, originalSpeaker, $index)' ng-model='text.speakerId' ng-options='speaker.id as speaker.name for speaker in speakers'></select>"
         };
 
         //edit transcripts

commit 3b7f858c27101eade92440124dbe81acc65e298a

rename popover template override for angular-strap since ng-bind-html appears to break our common cases. regression introduced in 5efb6a136bc5cfd67530a3c6f0b94c74dbba5ad2. see #1156

app/assets/javascripts/angular/services/templates.js
@@ -4,6 +4,7 @@ angular.module('mgcrea.ngStrap.dropdown').run([ '$templateCache', function($temp
     $templateCache.put('dropdown/dropdown.tpl.html', '<ul tabindex="-1" class="dropdown-menu" role="menu"><li role="presentation" ng-class="{divider: item.divider}" ng-repeat="item in content"><a role="menuitem" tabindex="-1" ng-href="{{item.href}}" ng-if="!item.divider && item.href && !item.method" target="{{item.target || \'\'}}" ng-bind="item.text"></a> <a tabindex="-1" ng-href="{{item.href}}" data-method="{{item.method || \'\'}}" ng-if="!item.divider && item.href && item.method" target="{{item.target || \'\'}}">{{item.text}}</a> <a role="menuitem" tabindex="-1" href="javascript:void(0)" ng-if="!item.divider && item.click" ng-click="$eval(item.click);$hide()" ng-bind="item.text"></a></li></ul>');
 } ]);
 
+// useful for embedded HTML within popover content
 angular.module('mgcrea.ngStrap.popover').run([ '$templateCache', function($templateCache) {
-    $templateCache.put('popover/popover.tpl.html', '<div class="popover" style="min-width:150px"><div class="arrow"></div><button type="button" class="close" ng-click="$hide()" aria-hidden="true">&times;</button><h3 class="popover-title" ng-bind="title" ng-show="title"></h3><div class="popover-content" ng-bind-html="content"></div></div>');
-  } ]);
+    $templateCache.put('popover/popover-html.tpl.html', '<div class="popover" style="min-width:150px"><div class="arrow"></div><button type="button" class="close" ng-click="$hide()" aria-hidden="true">&times;</button><h3 class="popover-title" ng-bind="title" ng-show="title"></h3><div class="popover-content" ng-bind-html="content"></div></div>');
+} ]);